00dac0b58bc58530ebc8ed9d95533a542f080b0c
sorawit chueachual <sorawit.c@kisracorp.com>
2025-10-28 14:45:35 +0700

    chore: school status

--- constants/enums.js
+++ constants/enums.js
@@ -2,4 +2,10 @@ const SCHOOL_TYPES = ["Public", "Private", "International"];
 
 const EDUCATION_LEVELS = ["Elementary", "Middle", "High"];
 
-module.exports = { SCHOOL_TYPES, EDUCATION_LEVELS };
+const SCHOOL_STATUSES = {
+  ACTIVE: "Active",
+  INACTIVE: "Inactive",
+  PENDING: "Pending",
+};
+
+module.exports = { SCHOOL_TYPES, EDUCATION_LEVELS, SCHOOL_STATUSES };
